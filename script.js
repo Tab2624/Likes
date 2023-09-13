021a93f66af58a359c53fe1b3a36bcbad5be3990
Tab2624 <tabodansky@gmail.com>
2023-09-13 17:32:04 -0400

    Likes Project

---
+++ script.js
@@ -0,0 +1,21 @@
+var count = 9
+function addLikes(idToSelect){
+    var elem = document.querySelector(idToSelect)
+    count++
+    elem.innerText = count + ' like(s)'
+}
+
+var count2 = 12
+function addLikes2(idToSelect){
+    var elem = document.querySelector(idToSelect)
+    count2++
+    elem.innerText = count2 + ' like(s)'
+}
+
+
+var count3 = 9
+function addLikes3(idToSelect){
+    var elem = document.querySelector(idToSelect)
+    count3++
+    elem.innerText = count3 + ' like(s)'
+}
